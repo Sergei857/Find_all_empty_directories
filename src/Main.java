c9eb60210eaecf36a09fa4e74ed2bb51038e5194
Sergei857 <32968405+Sergei857@users.noreply.github.com>
2023-01-05 18:17:20 +0300

    1.0

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("");
+        File path = new File("D:\\basedir");
+        find(path, str);
+        System.out.println("");
 
-        System.out.println("Hello w orld! "
-        );
+    }
+
+    public static StringBuilder find(File f, StringBuilder s) {
+        StringBuilder str_add = s;
+        if (f == null) {
+            return str_add;
+        }
+        for (File file : f.listFiles()) {
+            //System.out.println(file.toString());
+            if (file.listFiles()==null){
+                str_add.append(file.toString());
+                str_add.append("\n");
+                System.out.println(str_add);
+            } else {
+                find(file, str_add);
+            }
+
+
+
+        }
+
+        return str_add;
     }
 }
+
+
+
